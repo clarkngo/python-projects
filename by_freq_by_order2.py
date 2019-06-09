5b7e6eefc488cfd36b980dc9108b0f44600b42f7
Clark Ngo <clarkngo@gmail.com>
2019-06-09 15:08:01 -0700

    redo code without comments and add method

--- by_freq_by_order2.py
+++ by_freq_by_order2.py
@@ -1,51 +1,25 @@
-# Declare a dictionary 
-dict = {}
-frequency = 1
-input_list = [7,2,4,2,1,8,8,7,2,2]
 
-# add numbers to hashtable and count frequency 
-# also get the index of their first occurence
+def sort(arr):
+  dict = {}
+  frequency = 1
 
-# iterate over array
-for index, value in enumerate(input_list):
-  if not str(value) in dict:      # if value not in dictionary
-    # add arr value as 'key' and frequency and index as 'value' in dictionary
-    dict.update({str(value):[frequency,index]})   
-  else:
-    dict[str(value)][0] += 1  # if it exists, increment the frequency
+  # iterate over array
+  for index, value in enumerate(arr):
+    if not str(value) in dict:
+      dict.update({str(value):[frequency,index]})   
+    else:
+      dict[str(value)][0] += 1
 
-# dict = sorted(dict.items(), key = lambda kv:(kv[1], kv[0]))
+  new_list = []
 
-print("list to hashtable:")
-print(dict)
+  for key, value in dict.items():
+    new_list.append([int(key),value[0],value[1]])
 
-# dictionary can't be sorted!! I think??
+  from operator import itemgetter
+  new_list = sorted(new_list, key=itemgetter(1), reverse=True)
 
-# i'll put the dict in a list
-
-new_list = []
-
-# iterate over dictionary
-for key, value in dict.items():
-  # add key and values as a list in a list
-  new_list.append([int(key),value[0],value[1]])
-print("hashtable to list:")
-print(new_list)
-from operator import itemgetter
-# sort by each list by the values in their index 1 (which is the frequency)
-new_list = sorted(new_list, key=itemgetter(1), reverse=True)
-
-print("sort by first index (frequency) and order is kept the same:")
-print(new_list)
-
-solution_list = []
-
-# iterate over new_list
-for i in new_list: 
-    # loop condition of each list depends on index 1 (which is the frequency)
-    for j in range(i[1]):
-      solution_list.append(i[0])  # add only index 0 (which is the value)
-
-print(solution_list)
-# input_list = [8,2,4,2,1,7,8,7,2,2]
-# input_list = [7,2,4,2,1,8,8,7,2,2]
+  solution_list = []
+  for i in new_list: 
+      for j in range(i[1]):
+        solution_list.append(i[0])
+  return solution_list
