0c475263f71d3c3679ef19b100302210a9b20f91
Clark Ngo <clarkngo@gmail.com>
2019-06-10 22:13:28 -0700

    change test name

--- test_by_freq_by_order2.py
+++ test_by_freq_by_order2.py
@@ -2,19 +2,19 @@ import by_freq_by_order2 # code to test
 import unittest # unit test framework
 
 class Test_Combination1(unittest.TestCase):
-    def test_base_conversion(self):
+    def test_freq_by_order(self):
         self.assertEqual(by_freq_by_order2.sort([ 7, 8, 8, 4, 1 ,2, 2, 2, 2, 7]), [2, 2, 2, 2, 7, 7, 8, 8, 4, 1])
 
 class Test_Combination2(unittest.TestCase):
-    def test_base_conversion(self):
+    def test_freq_by_order(self):
         self.assertEqual(by_freq_by_order2.sort([ 7, 8, 4, 1 ,2, 2, 2, 2, 7, 8]), [2, 2, 2, 2, 7, 7, 8, 8, 4, 1])
 
 class Test_Combination3(unittest.TestCase):
-    def test_base_conversion(self):
+    def test_freq_by_order(self):
         self.assertEqual(by_freq_by_order2.sort([ 7, 8, 4, 1 ,2, 7, 8, 2, 2, 2]), [2, 2, 2, 2, 7, 7, 8, 8, 4, 1])
 
 class Test_Combination4(unittest.TestCase):
-    def test_base_conversion(self):
+    def test_freq_by_order(self):
         self.assertEqual(by_freq_by_order2.sort([ 7, 7, 8, 8, 4, 1 ,2, 2, 2, 2]), [2, 2, 2, 2, 7, 7, 8, 8, 4, 1])
 
 
