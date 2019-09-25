2570f900e93d2589f2142c0930fe70bc7ed7b3ad
Clark Ngo <clarkngo@gmail.com>
2019-09-24 22:15:06 -0700

    copied solution

---
+++ arrays/pascal_triangle.py
@@ -0,0 +1,41 @@
+# Source: https://leetcode.com/problems/pascals-triangle/
+
+# Pascal's Triangle
+
+# Given a non-negative integer numRows, generate the first numRows of Pascal's triangle.
+
+
+# In Pascal's triangle, each number is the sum of the two numbers directly above it.
+
+# Example:
+
+# Input: 5
+# Output:
+# [
+#      [1],
+#     [1,1],
+#    [1,2,1],
+#   [1,3,3,1],
+#  [1,4,6,4,1]
+# ]
+
+# Runtime: 16 ms, faster than 73.88% of Python online submissions for Pascal's Triangle.
+# Memory Usage: 11.8 MB, less than 13.33% of Python online submissions for Pascal's Triangle.
+
+class Solution:
+    def generate(self, num_rows):
+        triangle = []
+
+        for row_num in range(num_rows):
+            # The first and last row elements are always 1.
+            row = [None for _ in range(row_num+1)]
+            row[0], row[-1] = 1, 1
+
+            # Each triangle element is equal to the sum of the elements
+            # above-and-to-the-left and above-and-to-the-right.
+            for j in range(1, len(row)-1):
+                row[j] = triangle[row_num-1][j-1] + triangle[row_num-1][j]
+
+            triangle.append(row)
+
+        return triangle
