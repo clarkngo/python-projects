8db10bcb3bef3ee5eea712e23a78886474d3e728
Clark Ngo <clarkngo@gmail.com>
2019-10-03 11:31:27 +0800

    Submission for Module 3

---
+++ CS160/Module03/DeleteItForMe.py
@@ -0,0 +1,21 @@
+import os
+
+def deletedirectory(ans):
+    if ans.lower() == 'y' or ans.lower() == 'yes':
+        prev = os.chdir('..')
+        for i in range(4, 11):
+            # if directory already exists, skip to make the next one
+            try:
+                os.rmdir('Module{}'.format(i))
+            except FileExistsError:
+                continue
+        print("THERE YOU GO!")
+        lst = os.listdir(prev)
+        lst.sort()
+        print(', '.join(lst))
+    else:
+        print("Maybe next time :)")
+
+if __name__ == "__main__":
+    answer = input("do you want to delete Module 4-10 directories? (y/n): ")
+    deletedirectory(answer)
