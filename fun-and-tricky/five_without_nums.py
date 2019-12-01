9fda454a2d1ec897817170e24755292841bea899
Clark Ngo <clarkngo@gmail.com>
2019-12-01 01:32:11 -0800

    LOL fun fun

---
+++ fun-and-tricky/five_without_nums.py
@@ -0,0 +1,19 @@
+# Source: https://www.codewars.com/kata/5-without-numbers/python
+
+# 5 without numbers !!
+
+# Write a function that always returns 5
+
+# Sounds easy right? Just bear in mind that you can't use any of the following characters: 0123456789*+-/
+
+# Good luck :)
+
+def unusual_five():
+    string = 'aaaaa'
+    return len(string)
+
+def unusual_five():
+    return len("five!")
+
+
+unusual_five = lambda: ord("")
