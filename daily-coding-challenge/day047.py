ae17c189cba0e2f2cd8b3c435280ba0e7a215d5c
Clark Ngo <clarkngo@gmail.com>
2019-12-05 08:55:23 -0800

    solved using nested for loops

--- daily-coding-challenge/day047.py
+++ daily-coding-challenge/day047.py
@@ -23,16 +23,10 @@ def buy_and_sell(arr):
 
 print(buy_and_sell([9, 11, 8, 5, 7, 10]) == 5)
 
-
-
-
-
-
-
-
-
 # Solution
-# The brute force solution here is to iterate over our list of stock prices, and for each price, find the largest profit you can make from selling that stock price (with the formula future - current), and keep track of the largest profit we can find. That would look like this:
+# The brute force solution here is to iterate over our list of stock prices, and for each price,
+# find the largest profit you can make from selling that stock price (with the formula future - current),
+# and keep track of the largest profit we can find. That would look like this:
 
 # def buy_and_sell(arr):
 #     max_profit = 0
@@ -43,9 +37,16 @@ print(buy_and_sell([9, 11, 8, 5, 7, 10]) == 5)
 #     return max_profit
 # This would take O(N2). Can we speed this up?
 
-# The maximum profit comes from the greatest difference between the highest price and lowest price, where the higher price must come after the lower one. But if we see a high price x and then a higher price y afterwards, then we can always discard x. So, if we keep track of the highest price in the future for each variable, we can immediately find how much profit buying at that price can make.
+# The maximum profit comes from the greatest difference between the highest price and lowest price,
+# where the higher price must come after the lower one.
+# But if we see a high price x and then a higher price y afterwards, then we can always discard x.
+# So, if we keep track of the highest price in the future for each variable,
+# we can immediately find how much profit buying at that price can make.
 
-# That means we can look at the array backwards and always keep track of the highest price we've seen so far. Then, at each step, we can look at the current price and check how much profit we would have made buying at that price by comparing with our maximum price in the future. Then we only need to make one pass!
+# That means we can look at the array backwards and always keep track of the highest price we've seen so far.
+# Then, at each step, we can look at the current price and
+# check how much profit we would have made buying at that price by comparing with our maximum price in the future.
+# Then we only need to make one pass!
 
 # def buy_and_sell(arr):
 #     current_max, max_profit = 0, 0
