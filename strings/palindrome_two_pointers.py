ccf5123757d842877422441ddf9a8ee48678a22b
Clark Ngo <clarkngo@gmail.com>
2019-12-20 23:38:09 -0800

    palindrome

---
+++ strings/palindrome_two_pointers.py
@@ -0,0 +1,33 @@
+# https://leetcode.com/problems/valid-palindrome/
+
+# Valid Palindrome
+
+# Given a string, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
+
+# Note: For the purpose of this problem, we define empty string as valid palindrome.
+
+# Example 1:
+
+# Input: "A man, a plan, a canal: Panama"
+# Output: true
+# Example 2:
+
+# Input: "race a car"
+# Output: false
+
+# Runtime: 56 ms, faster than 53.24% of Python3 online submissions for Valid Palindrome.
+# Memory Usage: 13.3 MB, less than 84.52% of Python3 online submissions for Valid Palindrome.
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        l, r = 0, len(s) - 1
+        while l < r:
+            while l < r and not s[l].isalnum():
+                l += 1
+            while r > l and not s[r].isalnum():
+                r -= 1
+            if s[l].lower() != s[r].lower():
+                return False
+            l += 1
+            r -= 1
+        return True
