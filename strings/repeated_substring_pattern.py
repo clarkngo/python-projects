f51cf2b91e0d8d108e28c4276d322c1a3ce237c8
Clark Ngo <clarkngo@gmail.com>
2019-12-29 13:03:19 -0800

    comment out

--- strings/repeated_substring_pattern.py
+++ strings/repeated_substring_pattern.py
@@ -1,28 +1,28 @@
-https://leetcode.com/problems/repeated-substring-pattern/
+# https://leetcode.com/problems/repeated-substring-pattern/
 
-Repeated Substring Pattern
+# Repeated Substring Pattern
 
-Given a non-empty string check if it can be constructed by taking a substring of it and appending multiple copies of the substring together. You may assume the given string consists of lowercase English letters only and its length will not exceed 10000.
+# Given a non-empty string check if it can be constructed by taking a substring of it and appending multiple copies of the substring together. You may assume the given string consists of lowercase English letters only and its length will not exceed 10000.
 
 
 
-Example 1:
+# Example 1:
 
-Input: "abab"
-Output: True
-Explanation: It's the substring "ab" twice.
-Example 2:
+# Input: "abab"
+# Output: True
+# Explanation: It's the substring "ab" twice.
+# Example 2:
 
-Input: "aba"
-Output: False
-Example 3:
+# Input: "aba"
+# Output: False
+# Example 3:
 
-Input: "abcabcabcabc"
-Output: True
-Explanation: It's the substring "abc" four times. (And the substring "abcabc" twice.)
+# Input: "abcabcabcabc"
+# Output: True
+# Explanation: It's the substring "abc" four times. (And the substring "abcabc" twice.)
 
-Runtime: 24 ms, faster than 98.21% of Python3 online submissions for Repeated Substring Pattern.
-Memory Usage: 12.8 MB, less than 100.00% of Python3 online submissions for Repeated Substring Pattern.
+# Runtime: 24 ms, faster than 98.21% of Python3 online submissions for Repeated Substring Pattern.
+# Memory Usage: 12.8 MB, less than 100.00% of Python3 online submissions for Repeated Substring Pattern.
 
 class Solution:
     def repeatedSubstringPattern(self, s: str) -> bool:
