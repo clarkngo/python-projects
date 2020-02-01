d9025e308b99342cac8ff6647f866092a095297d
Clark Ngo <clarkngo@gmail.com>
2020-02-01 15:29:40 -0800

    cartesian plane

---
+++ math/min_time_visit_points.py
@@ -0,0 +1,39 @@
+# Minimum Time Visiting All Points
+# On a plane there are n points with integer coordinates points[i] = [xi, yi]. Your task is to find the minimum time in seconds to visit all points.
+
+# You can move according to the next rules:
+
+# In one second always you can either move vertically, horizontally by one unit or diagonally (it means to move one unit vertically and one unit horizontally in one second).
+# You have to visit the points in the same order as they appear in the array.
+
+# Example 1:
+
+# Input: points = [[1,1],[3,4],[-1,0]]
+# Output: 7
+# Explanation: One optimal path is [1,1] -> [2,2] -> [3,3] -> [3,4] -> [2,3] -> [1,2] -> [0,1] -> [-1,0]
+# Time from [1,1] to [3,4] = 3 seconds
+# Time from [3,4] to [-1,0] = 4 seconds
+# Total time = 7 seconds
+# Example 2:
+
+# Input: points = [[3,2],[-2,2]]
+# Output: 5
+
+
+# Constraints:
+
+# points.length == n
+# 1 <= n <= 100
+# points[i].length == 2
+# -1000 <= points[i][0], points[i][1] <= 1000
+
+# Runtime: 60 ms, faster than 56.24% of Python3 online submissions for Minimum Time Visiting All Points.
+# Memory Usage: 12.8 MB, less than 100.00% of Python3 online submissions for Minimum Time Visiting All Points.
+
+class Solution:
+    def minTimeToVisitAllPoints(self, points: List[List[int]]) -> int:
+        if len(points) == 1: return 0
+        time = 0
+        for i in range(1, len(points)):
+            time += max(abs(points[i][0] - points[i-1][0]), abs(points[i][1] - points[i-1][1]))
+        return time
