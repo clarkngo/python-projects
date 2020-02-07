71df5e936d6f2511ceec8530e812cb42b33310cb
Clark Ngo <clarkngo@gmail.com>
2020-02-07 01:21:11 -0800

    runtime

--- stacks_and_queues/remove_all_adjacent_duplicates.py
+++ stacks_and_queues/remove_all_adjacent_duplicates.py
@@ -8,14 +8,15 @@
 
 # Return the final string after all such duplicate removals have been made.  It is guaranteed the answer is unique.
 
-
-
 # Example 1:
 
 # Input: "abbaca"
 # Output: "ca"
 # Explanation:
 # For example, in "abbaca" we could remove "bb" since the letters are adjacent and equal, and this is the only possible move.  The result of this move is that the string is "aaca", of which only "aa" is possible, so the final string is "ca".
+
+# Runtime: 56 ms, faster than 97.72% of Python3 online submissions for Remove All Adjacent Duplicates In String.
+# Memory Usage: 12.8 MB, less than 100.00% of Python3 online submissions for Remove All Adjacent Duplicates In String.
 
 class Solution:
     def removeDuplicates(self, S: str) -> str:
